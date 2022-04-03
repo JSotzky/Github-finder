cee617c6bdb540a3e78d260a8b577f6a8b83f52f
Jason Sotzky <Jasonsotzky@gmail.com>
2022-04-03 14:14:23 -0400

    Added basic profile search funtions

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return <div className=''>
+      <h1 className="text-5xl mb-4 text-black">Github Finder</h1>
+      <p className=""> A Github API app to practice the MERN stack.</p>
+
+  </div>;
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserResults from '../components/users/UserResults';
+
+function Home() {
+  return <>
+      {/* SEARCH COMPONENT */}
+      <UserResults />
+  </>;
+}
+
+export default Home;
